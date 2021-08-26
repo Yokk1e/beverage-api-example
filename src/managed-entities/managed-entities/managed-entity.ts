35c1f5c9a0ba4711dad3dfa8694c38113a997477
satthabut <satthabut.o@gmail.com>
2021-08-27 00:20:06 +0700

    - [feat] add managed entity

---
+++ src/managed-entities/managed-entities/managed-entity.ts
@@ -0,0 +1,23 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    DeleteDateColumn,
+  } from 'typeorm';
+  
+  @Entity()
+  export class ManagedEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @CreateDateColumn()
+    createdDate: Date;
+  
+    @UpdateDateColumn()
+    updatedDate: Date;
+  
+    @DeleteDateColumn()
+    deletedDate: Date;
+  }
+  
